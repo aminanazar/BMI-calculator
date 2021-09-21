034f47933a436c470865ef745858ff8a7124ba5f
amina <aminanazar786@gmail.com>
2021-09-21 17:51:31 +0530

    second commit

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+
+  BottomButton({@required this.buttonTitle,@required this.onTap});
+
+  final String buttonTitle;
+  final Function onTap;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child:Center(
+          child: Text(buttonTitle,
+            style: kLargeTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 45.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
